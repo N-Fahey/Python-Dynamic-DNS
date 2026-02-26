3814149e58fe635d260073f73753cf47398f4bc8
Nick Fahey <nfahey@live.com.au>
2026-02-27 10:45:38 +1100

    Initial commit

---
+++ app.py
@@ -0,0 +1,21 @@
+import include.api as api
+
+def main():
+    # Check settings for zones / records to update
+
+    # Check that zones / records exist in API & key valid
+
+    # Get current WAN IP
+
+    # Check if matches previous IP - log & exit
+
+    # Attempt to update cloudflare configuration to new IP
+
+    # If failed - log & exit
+
+    # Log IP change & save new IP as current IP
+
+    pass
+
+if __name__ == '__main__':
+    main()
