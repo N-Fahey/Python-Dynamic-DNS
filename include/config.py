c06090e005572dbeb599484a0957cd493f6a897b
Nick Fahey <nfahey@live.com.au>
2026-02-27 11:13:50 +1100

    feat: Add load config function

---
+++ include/config.py
@@ -0,0 +1,21 @@
+import json
+
+def load_config(filepath:str):
+    '''Load Cloudflare sites to update DNS records for. Site should be configured as an A record
+    Parameters:
+        filepath (String): Path to sites configuration file
+    
+    Returns:
+        config (Dict): JSON decoded sites configuration
+        None: If error loading or decoding configuration
+    '''
+    try:
+        with open(filepath, 'r') as config_file:
+            config = json.load(config_file)
+        
+        return config
+    
+    except FileNotFoundError:
+        print("Unable to load sites configuration file: file doesn't exist")
+    except json.JSONDecodeError as e:
+        print(f"JSON decode error when importing sites configuration: {e}")
