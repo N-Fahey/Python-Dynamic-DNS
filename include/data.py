09104e09156fc1e5c9a3f855cb06ddd323a16956
Nick Fahey <nfahey@live.com.au>
2026-02-27 12:22:06 +1100

    feat: Add function to save IP to file

--- include/data.py
+++ include/data.py
@@ -1,6 +1,10 @@
-def load_ip(filepath: str):
+def load_ip(filepath: str) -> str:
     with open(filepath, 'a+') as ip_file:
         ip_file.seek(0)
         read_ip = ip_file.readline()
     
     return read_ip
+
+def save_ip(filepath: str, ip:str) -> None:
+    with open(filepath, 'w') as ip_file:
+        ip_file.write(ip)
